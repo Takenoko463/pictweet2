762f21b0b315764f3657420c12e1b905c631fc1a
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-03-29 16:41:59 +0900

    編集画面の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,6 +21,11 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  #編集対象のツイートをindex変数に代入
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
   private
 
   def tweet_params
